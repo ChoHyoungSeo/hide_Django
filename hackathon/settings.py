a4f69537a039270b815bebd2ccb0b37c9d603f19
hwangnakjoo <hwangknock@gmail.com>
2019-08-10 03:24:29 +0900

    first commit

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8n4xv6zemoj3i6nbw)vddud8how*vd$u+&63*8ne30-_3#eicq'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','8n4xv6zemoj3i6nbw)vddud8how*vd$u+&63*8ne30-_3#eicq')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
